f58df2d8a6bb1d20ffa826aa7d8494894eaff1f2
korawit plrandacha 56660019 <satanic1254@hotmail.com>
2016-10-08 15:55:15 +0700

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,63 @@
+# midterm_887342
+US_states = {"Alabama" => "AL",
+	"Alaska" => "AK",
+	"Arizona" => "AZ",
+	"Arkansas" => "AR",
+	"California" => "CA",
+	"Colorado" => "CO",
+	"Connecticut" => "CT",
+	"Delaware" => "DE",
+	"District of Columbia" => "DC",
+	"Florida" => "FL",
+	"Georgia" => "GA",
+	"Hawaii" => "HI",
+	"Idaho" => "ID",
+	"Illinois" => "IL",
+	"Indiana" => "IN",
+	"Iowa" => "IA",
+	"Kansas" => "KS",
+	"Kentucky" => "KY",
+	"Louisiana" => "LA",
+	"Maine" => "ME",
+	"Maryland" => "MD",
+	"Massachusetts" => "MA",
+	"Michigan" => "MI",
+	"Minnesota" => "MN",
+	"Mississippi" => "MS",
+	"Missouri" => "MO",
+	"Montana" => "MT",
+	"Nebraska" => "NE",
+	"Nevada" => "NV",
+	"New Hampshire" => "NH",
+	"New Jersey" => "NJ",
+	"New Mexico" => "NM",
+	"New York" => "NY",
+	"North Carolina" => "NC",
+	"North Dakota" => "ND",
+	"Ohio" => "OH",
+	"Oklahoma" => "OK",
+	"Oregon" => "OR",
+	"Pennsylvania" => "PA",
+	"Rhode Island" => "RI",
+	"South Carolina" => "SC",
+	"South Dakota" => "SD",
+	"Tennessee" => "TN",
+	"Texas" => "TX",
+	"Utah" => "UT",
+	"Vermont" => "VT",
+	"Virginia" => "VA",
+	"Washington" => "WA",
+	"West Virginia" => "WV",
+	"Wisconsin" => "WI",
+	"Wyoming" => "WY"}
+
+    
+
+puts " Process hash M , N , S"
+    
+puts "-----------------------------------"
+    
+US_states.each {|key,value| puts value if value[1] == "A" || value[1] == "E" || value[1] == "I" || value[1] == "O" || value[1] == "U"}
+    
+puts "-----------------------------------"
+ปล.ได้แต่ทำให้ตัวหลังออกเป็นตัวสระ
